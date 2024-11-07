ac184eea23d2798c4a0f7cfb800119227b351733
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-11-07 19:02:14 +0700

    Script to interact with dynamodb

--- dynamodb.py
+++ dynamodb.py
@@ -1,15 +1,27 @@
+from contextlib import contextmanager
+
 import boto3
 from decouple import config
 
 ROLE_ARN = config('ROLE_ARN')
 
+
+@contextmanager
 def assume_role(role_arn, session_name):
     sts = boto3.client('sts')
     response = sts.assume_role(
         RoleArn=role_arn,
         RoleSessionName=session_name
     )
-    return response['Credentials']
+    try:
+        yield response['Credentials']
+    finally:
+        sts_assumed_role = boto3.client('sts',
+                                        aws_access_key_id=response['Credentials']['AccessKeyId'],
+                                        aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+                                        aws_session_token=response['Credentials']['SessionToken'])
+        sts_assumed_role.revoke_role_session(RoleSessionName=session_name)
+
 
 def list_items_from_dynamodb(table_name, credentials):
     dynamodb = boto3.resource('dynamodb', aws_access_key_id=credentials['AccessKeyId'],
@@ -18,6 +30,7 @@ def list_items_from_dynamodb(table_name, credentials):
     table = dynamodb.Table(table_name)
     response = table.scan()
     return response['Items']
+
 
 def create_item_in_dynamodb(table_name, credentials, key_id, shorten_path, destination_url, click_count=0):
     dynamodb = boto3.resource('dynamodb', aws_access_key_id=credentials['AccessKeyId'],
@@ -47,11 +60,6 @@ def get_item(table_name, credentials, key_id):
     return response.get('Item')
 
 
-
-def main(role_arn, session_name, table_name):
-    credentials = assume_role(role_arn, session_name)
-    print(list_items_from_dynamodb(table_name, credentials))
-
 if __name__ == '__main__':
-    main(ROLE_ARN, "session_1", "yashl")
-
+    with assume_role(ROLE_ARN, "session_1") as credentials:
+        print(list_items_from_dynamodb(table_name="yashl", credentials=credentials))
