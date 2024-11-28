d3d230bf416906c69d594256bf8dc5139a2f79ea
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-11-28 19:24:07 +0700

    list key by user_id

---
+++ src/list_key.py
@@ -0,0 +1,24 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+
+def lambda_handler(event, context):
+    user_id = event['user_id']
+    keys = list_keys_by_user_id(user_id)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(keys)
+    }
+
+
+def list_keys_by_user_id(user_id):
+    dynamodb = boto3.resource('dynamodb')
+    table_name = 'yashl'
+    table = dynamodb.Table(table_name)
+
+    response = table.table.query(
+        IndexName='user_id-index',
+        KeyConditionExpression=Key("user_id").eq(user_id)
+    )
+    return response['Items']
