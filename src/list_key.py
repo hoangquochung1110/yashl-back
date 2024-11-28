e25564154ee4b8017d5aa46ff733118ff7482c82
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-11-28 23:45:26 +0700

    Serialize Decimal

--- src/list_key.py
+++ src/list_key.py
@@ -1,15 +1,24 @@
+from decimal import Decimal
 import json
 import boto3
 from boto3.dynamodb.conditions import Key
 
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)  # or float(obj)
+        return super().default(obj)
+
+
 def lambda_handler(event, context):
     """Lambda to handle list key request."""
-    user_id = event['user_id']
+    query_params = event['queryStringParameters']
+    user_id = query_params['user_id']
     keys = list_keys_by_user_id(user_id)
     return {
         'statusCode': 200,
-        'body': json.dumps(keys)
+        'body': json.dumps(keys, cls=DecimalEncoder)
     }
 
 
@@ -18,7 +27,7 @@ def list_keys_by_user_id(user_id):
     table_name = 'yashl'
     table = dynamodb.Table(table_name)
 
-    response = table.table.query(
+    response = table.query(
         IndexName='user_id-index',
         KeyConditionExpression=Key("user_id").eq(user_id)
     )
