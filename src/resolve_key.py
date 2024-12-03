df89794dd44392f17c01d3327eb6c1248b8e21a2
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-12-03 14:59:23 +0700

    Enabling CORS support for proxy integrations

--- src/resolve_key.py
+++ src/resolve_key.py
@@ -23,6 +23,11 @@ def lambda_handler(event, context):
 
     return {    
             'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
             'body': json.dumps({
                 'destination_url': destination_url,
             }),
