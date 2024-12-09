d38ee1384670f1651a5325474e81dadc0ac5c3e9
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-12-09 09:58:05 +0700

    Fix argument missing

--- src/capture_screenshot.py
+++ src/capture_screenshot.py
@@ -2,7 +2,7 @@ import shutil
 from screenshotone import Client, TakeOptions
 
 
-def lambda_handler():
+def lambda_handler(event, context):
     """Capture a screenshot."""
     return {
         "statusCode": 200,
