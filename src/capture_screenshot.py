2f3cacd5ebca078bc97e0ef46a5a6b82916da298
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-12-09 09:53:09 +0700

    Rename function to lambda_handler

--- src/capture_screenshot.py
+++ src/capture_screenshot.py
@@ -2,7 +2,7 @@ import shutil
 from screenshotone import Client, TakeOptions
 
 
-def capture_screenshot():
+def lambda_handler():
     """Capture a screenshot."""
     return {
         "statusCode": 200,
